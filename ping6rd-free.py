5a98a84c507a3253b589011b70dc45c75afae58f
Bruno STEVANT <bruno.stevant@telecom-bretagne.eu>
2017-03-26 21:50:50 +0200

    First working version

---
+++ ping6rd-free.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python 
+
+from __future__ import unicode_literals
+import ipaddress
+from python_ping import ping
+
+
+# from lafibre.info
+# https://lafibre.info/ipv6/plages-ip-par-fai/msg87273/
+#Free (AS12322) => 11 141 114 IPv4
+#212.27.32.0/19    AS12322
+#213.228.0.0/18    AS12322
+#62.147.0.0/16     AS12322
+#81.56.0.0/15      AS12322
+#82.64.0.0/14      AS12322
+#82.224.0.0/11     AS12322
+#88.160.0.0/11     AS12322
+#78.192.0.0/10     AS12322
+#83.152.0.0/13     AS12322
+#88.120.0.0/13     AS12322
+#91.160.0.0/12     AS12322
+#213.36.0.0/16     AS12322
+#83.214.0.0/16     AS12322
+#194.149.160.0/19  AS12322
+#82.142.0.0/18     AS12322
+#212.62.128.0/18   AS12322
+
+
+FREE_6RD_PREFIX = "2a01:e30::"
+
+def ipv4_to_ipv6rd(ipv4_addr, sixrd_pref):
+    # Free 6rd prefix
+    sixrd_prefix = ipaddress.ip_address(sixrd_pref)
+    # Shift 68 times the 32b of IPv4 address
+    ip4rd = int(ipv4_addr) << 68
+    # Binary OR with 6rd prefix
+    long_prefix = int(sixrd_prefix) | ip4rd
+    # Add ::1 to get the address of the freebox
+    sixrd_address = ipaddress.ip_address(long_prefix + 1)
+    return sixrd_address
+
+
+
+ipv6pref_scanlist = [
+    '2a01:e35:8780::/43', # ADSL: 6rd prefix for 88.120.0.0/13
+    '2a01:e0a:2::/47' # Fiber: Native IPv6
+]
+
+
+
+for ipv6pref in ipv6pref_scanlist:
+    p = ipaddress.IPv6Network(ipv6pref)
+    for freebox_ipv6pref in p.subnets(new_prefix=60):
+        freebox_ipv6addr = freebox_ipv6pref.hosts().next()
+        #print freebox_ipv6addr
+        try:
+            ping_res = list(ping.quiet_ping(str(freebox_ipv6addr), timeout=3000, count=10, ipv6=True))
+            rttmax, rttmin, rttavg, lost =  ping_res[-1]
+            print rttavg
+        except:
+            continue
+
+#for ipv4_pref, sixrd_pref in six6rd_plan.iteritems():
+#    free_net = ipaddress.ip_network(ipv4_pref)
+#    for ipv4_addr in free_net:
+#        sixrd_addr = ipv4_to_ipv6rd(ipv4_addr, sixrd_pref)
+#        print sixrd_addr
+#        try:
+#            p = list(ping.quiet_ping(str(sixrd_addr), timeout=3000, count=3, ipv6=True))
+#            print p[-1]
+#        except:
+#            continue
